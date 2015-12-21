e5c2b67f0e3271a277d8d58c1e11f5622b3b9243
Enric <46989414t@iespoblenou.org>
2015-12-21 18:03:35 +0100

    lista video semi ok

--- app/src/main/java/com/bikeworld/bikeworld/MyVideos.java
+++ app/src/main/java/com/bikeworld/bikeworld/MyVideos.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -29,6 +30,8 @@ public class MyVideos extends UserProfile {
     //comprobar proyecto carlos: https://github.com/lawer/RottenTomatoesClient/blob/f8e78c5c9828cd661ad64f203fb5f964e750fac0/app/src/main/java/poblenou/rottentomatoesclient/MoviesAdapter.java
 
     public ArrayList<VideoObject> datos;
+
+    public VideosAdapterList adapter;
 
     private ListView lista;
     public EditText url;
@@ -69,30 +72,162 @@ public class MyVideos extends UserProfile {
 
         //para hacer los gets
 
-            final Firebase referencia = new Firebase("https://dazzling-inferno-4414.firebaseio.com/bikeWorld/videos/");
-            referencia.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                        VideoObject video = postSnapshot.getValue(VideoObject.class);
-                        if(video.getUserName().equals(logUser)){
-                            datos.add(video);
-                        }
-                        //datos.add(video);
-                        System.out.println("Los videos guardados" + video);
-                        System.out.println("Los videos a mostrar: "+datos);
-                    }
-                }
-                @Override
-                public void onCancelled(FirebaseError firebaseError) {
-
-                }
-            });
+        final Firebase referencia = new Firebase("https://dazzling-inferno-4414.firebaseio.com/bikeWorld/videos/");
 
         lista = (ListView) findViewById(R.id.listaVideos);
+        //final VideosAdapterList adapter = new VideosAdapterList(this, datos);
+        adapter = new VideosAdapterList(this, datos);
 
-        VideosAdapterList adapter = new VideosAdapterList(this, datos);
         lista.setAdapter(adapter);
+
+        /*referencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    VideoObject video = postSnapshot.getValue(VideoObject.class);
+                    if (video.getUserName().equals(logUser)) {
+                        datos.add(video);
+                    }
+                    //datos.add(video);
+                    System.out.println("Los videos guardados" + video);
+                    System.out.println("Los videos a mostrar: " + datos);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });*/
+
+
+        referencia.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                adapter.notifyDataSetChanged();
+                System.out.println("ha actualizado los datos del adapter");
+                //for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    VideoObject video = dataSnapshot.getValue(VideoObject.class);
+                    if (video.getUserName().equals(logUser)) {
+                        datos.add(video);
+                   // }
+                    //datos.add(video);
+                    System.out.println("Los videos guardados" + video);
+                    System.out.println("Los videos a mostrar: " + datos);
+                        /*adapter.notifyDataSetChanged();
+                        System.out.println("ha actualizado los datos del adapter");*/
+                }
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                adapter.notifyDataSetChanged();
+                System.out.println("ha actualizado los datos del adapter");
+                /*for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    VideoObject video = postSnapshot.getValue(VideoObject.class);
+                    if (video.getUserName().equals(logUser)) {
+                        datos.add(video);
+                    }
+                    //datos.add(video);
+                    System.out.println("Los videos guardados" + video);
+                    System.out.println("Los videos a mostrar: " + datos);
+                }
+                //adapter.notifyDataSetChanged();
+                //System.out.println("ha actualizado los datos del adapter");
+                /*VideoObject video = dataSnapshot.getValue(VideoObject.class);
+                if (video.getUserName().equals(logUser)) {
+                    datos.add(video);
+
+                }
+                //datos.add(video);
+                System.out.println("Los videos guardados" + video);
+                System.out.println("Los videos a mostrar: " + datos);*/
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+       referencia.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                /*for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    VideoObject video = postSnapshot.getValue(VideoObject.class);
+                    if (video.getUserName().equals(logUser)) {
+                        datos.add(video);
+                    }
+                    //datos.add(video);
+                    System.out.println("Los videos guardados" + video);
+                    System.out.println("Los videos a mostrar: " + datos);
+                }*/
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+        /*listView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                // Tu código para recargar los datos
+
+                // ...
+
+                // Asegurate de llamar a listView.onRefreshComplete()
+
+                listView.onRefreshComplete();
+            }
+        });*/
+
+
+
+        //lista.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+        //});
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                final VideoObject elegido = (VideoObject) parent.getItemAtPosition(position);
+                System.out.println("Llega al click largo con Objeto seleccionado: " + elegido);
+                dialogoEliminar.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //elimina el objeto
+                        datos.remove(elegido);
+                        //lo elimina de la base de datos
+                        referencia.child("videoObject_" + elegido.getTitle().toString()).removeValue();
+
+                        adapter.notifyDataSetChanged();
+                        System.out.println("ha actualizado los datos del adapter");
+                    }
+                });
+                dialogoEliminar.show();
+
+                return true;
+            }
+        });
 
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -106,28 +241,11 @@ public class MyVideos extends UserProfile {
 
             }
         });
-        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                final VideoObject elegido = (VideoObject) parent.getItemAtPosition(position);
-                System.out.println("Llega al click largo con Objeto seleccionado: " + elegido);
-                dialogoEliminar.setPositiveButton("Eliminar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        datos.remove(elegido);
-                    }
-                });
-                dialogoEliminar.show();
-
-                //datos.remove(elegido);
-
-                return false;
-            }
-        });
 
     }
 
     public void onAddVideo(View view) {
+
         url = (EditText) findViewById(R.id.idURL);
         titulo=(EditText) findViewById(R.id.idTitulo);
         descripcion = (EditText) findViewById(R.id.idDescription);
@@ -151,8 +269,10 @@ public class MyVideos extends UserProfile {
 
         System.out.println("objeto de video añadido a la BBDD");
         datos.add(videoObject);
+        adapter.notifyDataSetChanged();
         System.out.println("El video añadido es: " + videoObject.getTitle());
-        System.out.println("imprimir DATOS: "+datos);
+        System.out.println("imprimir DATOS: " + datos);
+
 
     }
 }
